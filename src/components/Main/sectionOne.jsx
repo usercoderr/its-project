0d49b57c40a52351e0403b14f9d86cfc795d627b
muratiskenov <morat.aisken@gmail.com>
2022-11-30 18:08:47 +0600

    add section-one animations

--- src/components/Main/sectionOne.jsx
+++ src/components/Main/sectionOne.jsx
@@ -1,25 +1,111 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
 import sectionOne from "../../assets/sectionOne.svg";
 import "./styles/sectionOne.css";
 
 export default function SectionOne() {
-  const {t} = useTranslation()
+  const spanVariants = {
+    hidden:{
+      x: -1000,
+      opacity: 0,
+    },
+    visible:{
+      x: 100,
+      x: 0,
+      opacity:1,
+    },
+  }
+  const headingVariants ={
+    hidden:{
+      x: -500,
+      opacity: 0,
+    },
+    visible:{
+      x: 0,
+      opacity:1,
+    },
+  }
+  const linkVariants = {
+    hidden:{
+      opacity:0
+    },
+    visible:{
+      opacity:1
+    }
 
+  }
+  const blockVariants = {
+    hidden:{
+      x:1000,
+      opacity:0
+    },
+    visible:{
+      x:0,
+      opacity:1
+    },
+  }
+
+  const {t} = useTranslation()
   return (
     <section className="section-one">
       <div className="section-one_inside">
         <div className="section-one_inside-left">
-          <span>{t("development")}</span>
-          <h1>{t("digital")}</h1>
-          <div className="section-one_button">
-            <span>{t("send")}</span>
+          <motion.span
+            initial={'hidden'}
+            animate={'visible'}
+            transition={{
+              delay:1,
+              duration:1.5,
+            }}
+            variants={spanVariants}
+          >
+            {t("development")}
+          </motion.span>
+          <motion.h1
+            initial={'hidden'}
+            animate={'visible'}
+            transition={{
+              delay:1,
+              duration:1.5,
+          }}
+          variants={headingVariants}
+
+          >
+            {t("digital")}</motion.h1>
+          <motion.div 
+            className="section-one_button"
+            initial={'hidden'}
+            animate={'visible'}
+            transition={{
+              delay:0.5,
+              duration:2
+            }}
+            variants={linkVariants}
+            whileHover={{
+              x:75,
+              duration:0.5,
+              scale:1.2,
+            }}
+          >
+            <span>
+              {t("send")}
+            </span>
             <div className="border-line-button"></div>
-          </div>
+          </motion.div>
         </div>
-        <div className="section-one_inside-right">
+        <motion.div 
+          className="section-one_inside-right"
+          initial={'hidden'}
+          animate={'visible'}
+          transition={{
+            delay:1,
+            duration:1.5,
+          }}
+          variants={blockVariants}
+        >
           <img src={sectionOne} alt="" />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
