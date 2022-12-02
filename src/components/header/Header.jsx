8c2e0ccda1deb88ffab902b396206338d89e87b7
muratiskenov <morat.aisken@gmail.com>
2022-12-02 11:30:49 +0600

    header-logo animated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import logo from "../../assets/logo.svg";
+import { motion } from "framer-motion";
 import Lang from "../../features/Lang";
+import { type } from "@testing-library/user-event/dist/type";
 
 export default function Header() {
   const { t } = useTranslation()
+  const logoVariants = {
+    hidden:{
+      x:-500,
+      opacity:0
+    },
+    visible:{
+      x:0,
+      opacity:1
+      
+    }
+  }
   return (
     <header>
       <div className="header">
-        <a href="#">
+        <motion.a
+          initial='hidden'
+          animate='visible'
+          transition={{
+            delay:0.7,
+            duration:1,
+            type:"spring"
+          }}
+          variants={logoVariants} 
+
+          href="#">
         {/* width="104" height="93"  */}
           <img src={logo} alt="" />
-        </a>
+        </motion.a>
         <ul>
           <li>
             <a href="#">{t("main")}</a>
