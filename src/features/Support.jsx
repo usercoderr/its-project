57c1515a5e1625486b58979653a4bc439b0246a3
aktan <kerimbekov1233@gmail.com>
2022-12-03 00:02:05 +0600

    contact show mpre

--- src/features/Support.jsx
+++ src/features/Support.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import {RiTelegramLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
-import {BsFillTelephoneFill} from 'react-icons/bs'
-import {GrMail} from 'react-icons/gr'
+import { RiTelegramLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+import { BsFillTelephoneFill } from 'react-icons/bs'
+import { GrMail } from 'react-icons/gr'
 import message from "../assets/message.svg";
 import "./support.css";
 
 export default function Support() {
   const [isOpen, setOpen] = useState();
-  const handleClickModal = () =>{
+  const handleClickModal = () => {
     setOpen(!isOpen)
   }
   return (
@@ -20,29 +20,27 @@ export default function Support() {
       </div>
       {
         isOpen && <div className="modal">
-        <div className="modal-inside">
+          <div className="modal-inside">
             <h3>Напишите нам</h3>
-            <span>Узнайте подробнее о курсах или запишитесь на пробное занятие</span>
             <div className="modal-icons">
-
-            <div className="icons">
-                <RiTelegramLine color="#229ED9." fontSize='50px'/>
+              <div className="icons">
+                <RiTelegramLine color="#0088cc" fontSize='50px' />
                 <p>Telegram</p>
-            </div>
-            <div className="icons">
-                <BsWhatsapp color="#075E54" fontSize='50px'/>
+              </div>
+              <div className="icons">
+                <BsWhatsapp color="#075E54" fontSize='44px' style={{ margin: '3px' }} />
                 <p>What's App</p>
-            </div>
-            <div className="icons" fontSize='50px'>
-                <GrMail fontSize='50px'/>
+              </div>
+              <div className="icons" fontSize='50px'>
+                <GrMail fontSize='50px' />
                 <p>Mail</p>
-            </div>
-            <div className="icons phone">
-                <BsFillTelephoneFill fontSize='50px'/>
+              </div>
+              <div className="icons phone">
+                <BsFillTelephoneFill fontSize='45px' />
                 <p>Phone</p>
+              </div>
             </div>
-            </div>
-        </div>
+          </div>
         </div>
       }
     </>
