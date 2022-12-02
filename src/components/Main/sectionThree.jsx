6d275083d039d424a400c285fc4c15f513e51c6c
aktan <kerimbekov1233@gmail.com>
2022-12-03 03:41:20 +0600

    mobie adaptive

--- src/components/Main/sectionThree.jsx
+++ src/components/Main/sectionThree.jsx
@@ -2,52 +2,88 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
 import "./styles/sectionThree.css";
+import Carousel from 'better-react-carousel'
 
 export default function SectionThree() {
-  const {t} = useTranslation()
-  
+  const { t } = useTranslation()
 
-    const [isOpen, setOpen] = useState(false)
-    const handleClick = () =>{
-        setOpen(!isOpen)
+
+  const [isOpen, setOpen] = useState(false)
+  const handleClick = () => {
+    setOpen(!isOpen)
+  }
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
     }
+  };
   return (
     <section id="portfolio" className="section-three">
       <div className="section-three_inside">
         <div className="section-three_inside-block">
           <h3>{t("products")}</h3>
-          <div className="angry-grid">
-            <div id="item-0">&nbsp;</div>
-            <div id="item-1">&nbsp;</div>
-            <div id="item-2">&nbsp;</div>
-            <div id="item-3">&nbsp;</div>
-            <div id="item-4">&nbsp;</div>
-            <div id="item-5">&nbsp;</div>
-            <div id="item-6">&nbsp;</div>
-            <div id="item-7">&nbsp;</div>
+          <div className="grid-container">
+            <div className="angry-grid">
+              <div id="item-0">&nbsp;</div>
+              <div id="item-1">&nbsp;</div>
+              <div id="item-2">&nbsp;</div>
+              <div id="item-3">&nbsp;</div>
+              <div id="item-4">&nbsp;</div>
+              <div id="item-5">&nbsp;</div>
+              <div id="item-6">&nbsp;</div>
+              <div id="item-7">&nbsp;</div>
+            </div>
+            <br />
+            {
+              !isOpen &&
+              <button onClick={handleClick}>{t("showButton")}</button>
+            }
+            {
+              isOpen ? <div class="angry-grid">
+                <div id="item-0">&nbsp;</div>
+                <div id="item-1">&nbsp;</div>
+                <div id="item-2">&nbsp;</div>
+                <div id="item-3">&nbsp;</div>
+                <div id="item-4">&nbsp;</div>
+                <div id="item-5">&nbsp;</div>
+                <div id="item-6">&nbsp;</div>
+                <div id="item-7">&nbsp;</div>
+              </div> : ''
+            }
+            <br />
+            {
+              isOpen &&
+              <button onClick={handleClick}>{t("hiddenButton")}</button>
+            }
           </div>
-          <br />
-         {
-            !isOpen &&
-         <button onClick={handleClick}>{t("showButton")}</button>
-         } 
-          {
-            isOpen ? <div class="angry-grid">
-            <div id="item-0">&nbsp;</div>
-            <div id="item-1">&nbsp;</div>
-            <div id="item-2">&nbsp;</div>
-            <div id="item-3">&nbsp;</div>
-            <div id="item-4">&nbsp;</div>
-            <div id="item-5">&nbsp;</div>
-            <div id="item-6">&nbsp;</div>
-            <div id="item-7">&nbsp;</div>
-          </div> : ''
-          }
-          <br />
-          {
-            isOpen && 
-         <button onClick={handleClick}>{t("hiddenButton")}</button>
-          }
+
+          <div className="carousel">
+            <Carousel cols={1} rows={1} gap={10} scrollSnap={true} loop mobileBreakpoint={767}>
+              <Carousel.Item>
+                <img width="100%" src="https://picsum.photos/800/600?random=1" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img width="100%" src="https://picsum.photos/800/600?random=2" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img width="100%" src="https://picsum.photos/800/600?random=3" />
+              </Carousel.Item>
+            </Carousel>
+          </div>
 
         </div>
       </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import styled from "styled-components";
 import logo from "../../assets/logo.svg";
 import { motion } from "framer-motion";
 import Lang from "../../features/Lang";
-import { type } from "@testing-library/user-event/dist/type";
-import { slide as Menu } from 'react-burger-menu';
+import { MenuToggle } from "./menuToggle";
+
+
+
+const NavLinksContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const LinksWrapper = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  height: 100%;
+  list-style: none;
+  background-color: #fff;
+  width: 100%;
+  flex-direction: column;
+  position: fixed;
+  top: 60px;
+  right: 0;
+  z-index: 11;
+`;
+
+const LinkItem = styled.li`
+  width: 100%;
+  padding: 0 1.1em;
+  color: #222;
+  font-weight: 500;
+  font-size: 16px;
+  display: flex;
+
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: inherit;
+  font-size: inherit;
+`;
 
 export default function Header() {
   const { t } = useTranslation();
@@ -18,6 +58,11 @@ export default function Header() {
       opacity: 1,
     },
   };
+
+  const [isOpen, setOpen] = useState(false);
+
+
+
   return (
     <header>
       <div className="header">
@@ -35,7 +80,7 @@ export default function Header() {
           {/* width="104" height="93"  */}
           <img src={logo} alt="" />
         </motion.a>
-        <ul>
+        <ul className="desctop">
           <a href="#">
             <li>{t("main")}</li>
           </a>
@@ -49,28 +94,57 @@ export default function Header() {
             <li>{t("contacts")}</li>
           </a>
         </ul>
-        <Menu>
-          <a href="#" className="menu-item">
-            <li>{t("main")}</li>
-          </a>
-          <a href="#about_us" className="menu-item">
-            <li>{t("about_us")}</li>
-          </a>
-          <a href="#portfolio" className="menu-item">
-            <li>{t("portfolio")}</li>
-          </a>
-          <a href="#contacts" className="menu-item">
-            <li>{t("contacts")}</li>
-          </a>
-          <a className="menu-item">
-            <Lang/>
-          </a>
-        </Menu>
-        <div className="langChang">
 
-        <Lang />
+
+        <NavLinksContainer>
+          <MenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
+          {isOpen && (
+            <div className="burger-container">
+              <LinksWrapper>
+                <LinkItem>
+                  <Link onClick={() => setOpen(!isOpen)}>
+                    <a href="#" onClick={() => setOpen(!isOpen)}>
+                      <li>{t("main")}</li>
+                    </a>
+                  </Link>
+                </LinkItem>
+                <LinkItem>
+                  <Link onClick={() => setOpen(!isOpen)}>
+                    <a href="#about_us" onClick={() => setOpen(!isOpen)}>
+                      <li>{t("about_us")}</li>
+                    </a>
+                  </Link>
+                </LinkItem>
+                <LinkItem>
+                  <Link onClick={() => setOpen(!isOpen)}>
+                    <a href="#portfolio" onClick={() => setOpen(!isOpen)}>
+                      <li>{t("portfolio")}</li>
+                    </a>
+                  </Link>
+                </LinkItem>
+                <LinkItem>
+                  <Link onClick={() => setOpen(!isOpen)}>
+                    <a href="#contacts" onClick={() => setOpen(!isOpen)}>
+                      <li>{t("contacts")}</li>
+                    </a>
+                  </Link>
+                </LinkItem>
+                <LinkItem>
+                  <Link href="#" className="langChageBurger">
+                    <Lang />
+                  </Link>
+                </LinkItem>
+              </LinksWrapper>
+            </div>
+          )}
+        </NavLinksContainer>
+
+
+        <div className="langChang">
+          <Lang />
         </div>
       </div>
     </header>
   );
 }
+
